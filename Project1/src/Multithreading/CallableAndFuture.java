3d3807724d4b3fbb8941e9497f19757b7a3f6bf7
Diyor <diyorp2@mail.ru>
2022-06-09 14:21:13 +0600

    CallableAndFuture

---
+++ Project1/src/Multithreading/CallableAndFuture.java
@@ -0,0 +1,41 @@
+package Multithreading;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class CallableAndFuture {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<Integer> future = executorService.submit(new Callable<Integer>() { //Callable возвращает значение, а Runnable нет
+
+            @Override
+            public Integer call() throws Exception {
+                System.out.println("Starting");
+                try{
+                    Thread.sleep(500);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("Finished");
+                Random random = new Random();
+                int randomValue = random.nextInt();
+                if(randomValue < 5)
+                    throw new Exception("Smth bad happened");
+
+                return random.nextInt(10);
+            }
+        });
+
+        executorService.shutdown();
+        try {
+            int result = future.get(); // get ожидает окончания выполнения потока
+            System.out.println(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e){
+            Throwable ex = e.getCause();
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
